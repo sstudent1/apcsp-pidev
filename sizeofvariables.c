4c183e96516adce505f41656a9efdab4baaa00a8
sstudent1 <sadietoomey@gmail.com>
2018-06-01 15:03:16 +0000

    commit

---
+++ sizeofvariables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 10;
+	float b = 2.31;
+	double c = 42;
+	char d = 6;
+printf("Var a has a size of %d bytes\n,b has a size of %d bytes\n,c has a size of %d bytes\n,d has a size of %d bytes\n", sizeof(a)), sizeof(b), sizeof(c), sizeof(d);
+}
+ 
