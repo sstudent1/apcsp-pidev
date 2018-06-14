68d054492ceadac99829f1c921960dacc2fc2ef8
sstudent1 <sadietoomey@gmail.com>
2018-06-14 16:02:21 +0000

    c activites makefile

--- myproject/student.h
+++ myproject/student.h
@@ -1,2 +1,10 @@
-#include <stdio.h>
-#include <string.h>
+struct Students {
+        char  firstname[50];
+        char  lastname[50];
+        int age;
+        int studentid;
+};
+
+void printStudent(struct Students* student);
+
+
